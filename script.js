a9ba9acce900e222d5051a2d520e10b29df6d0ba
Kayla Hunt <huntkay1@msu.edu>
2024-02-21 19:51:17 -0500

    Add dialog modal for form
    The book info form pops up when the add book button is pressed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
-cardContainer = document.getElementById("card-container");
+const cardContainer = document.getElementById("card-container");
+const addBookBttn = document.getElementById("add-book");
+const dialog = document.querySelector("dialog");
+const closeBttn = document.getElementById("close");
 
 //Creates book object
 function Book(title, author, pages, read) {
@@ -11,7 +14,7 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(ev) {
     ev.preventDefault(); //stops the form from submitting - ev is the event
-    let book = new Book(
+    const book = new Book(
         document.getElementById('title').value,
         document.getElementById('author').value,
         document.getElementById('pages').value,
@@ -23,23 +26,11 @@ function addBookToLibrary(ev) {
     makeCard();
 }
 
-// function createCard() {
-//     card = document.createElement("div");
-//     author = document.createElement("h2");
-//     title = document.createElement("h3")
-//     pages = document.createElement("p");
-//     read = document.createElement("p");
-//     card.classList.add("card");
-//     card.appendChild(author);
-//     card.appendChild(title);
-//     card.appendChild(pages);
-//     card.appendChild(read);
-//     cardContainer.appendChild(card);
-// }
-
 function makeCard() {
-   myLibrary.forEach((element, index) => {
+   cardContainer.innerHTML = ''; //clear the container after each addition to prevent repeats
 
+   myLibrary.forEach((element, index) => {
+        //make the card elements
         card = document.createElement("div");
         author = document.createElement("h2");
         title = document.createElement("h3")
@@ -52,7 +43,7 @@ function makeCard() {
         card.appendChild(read);
         cardContainer.appendChild(card);
 
-
+        //add card text contents
         title.innerHTML = element.title;
         author.innerHTML = element.author;
         pages.innerHTML = element.pages;
@@ -61,10 +52,17 @@ function makeCard() {
 }
 
 
-
 //when content is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('submit').addEventListener('click', addBookToLibrary);
+})
+
+addBookBttn.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+close.closeBttn.addEventListener("click", () => {
+    dialog.close;
 })
 
 
